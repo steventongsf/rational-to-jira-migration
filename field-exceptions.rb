d16284197c27c4255a39d3002896b91827c508af
stevenstong <steven.tong@hcl.com>
2020-02-24 11:42:53 -0800

    debugging

--- field-exceptions.rb
+++ field-exceptions.rb
@@ -23,24 +23,30 @@ end
 def test_mappings(id,key_num,hash,rtcrow,jirarow,rtctext,jiratext)
   msg = nil
   rtcrow[key_num] = "" if rtcrow[key_num] == nil
-  if !hash.include?(rtcrow[key_num])
-    msg = rtctext+" value not in mapping. value:"+rtcrow[key_num]+",id:"+id+",moddate:"+rtcrow[1]
+  hash_key = rtcrow[key_num].strip
+  if !hash.include?(hash_key)
+    msg = rtctext+" value not in mapping. key:"+hash_key+",id:"+id+",moddate:"+rtcrow[1]
   else
     if jiratext =~ /jira user/
-      if rtcrow[key_num] == "" && (jirarow[key_num] != "rtcuser" || jirarow[key_num] == "" || jirarow[key_num] == nil)
+      if hash_key == "" && (jirarow[key_num] != "rtcuser" || jirarow[key_num] == "" || jirarow[key_num] == nil)
         msg = "Empty user should map to rtcuser or empty. Id:"+id
       end
-    elsif hash[rtcrow[key_num]] == nil  
-      puts "Key: '"+ rtcrow[key_num]+"'"
-      msg = jiratext+" value not found. Key:#{rtcrow[key_num]},jira actual:"+ jirarow[key_num]+",id:"+id+",moddate:"+rtcrow[1]
+    elsif hash[hash_key] == nil  
+      msg = jiratext+" value not found. Key:#{hash_key},jira actual:"+ jirarow[key_num]+",id:"+id+",moddate:"+rtcrow[1]
   
     elsif jirarow[key_num] ==nil
       msg = jiratext+" not in mapping. Expected:Unknown,actual:Unknown,id:"+id+",moddate:"+rtcrow[1]
-    elsif hash[rtcrow[key_num]].downcase.tr("()","") != jirarow[key_num].downcase.tr("()","")
-      msg = jiratext+" doesn't match mapping. Key:#{rtcrow[key_num]} Expected:"+hash[rtcrow[key_num]]+",actual:"+ jirarow[key_num]+",id:"+id+",moddate:"+rtcrow[1]
+    elsif hash[hash_key].downcase.tr("()","") != jirarow[key_num].downcase.tr("()","")
+      msg = jiratext+" doesn't match mapping. Key:#{hash_key} Expected:"+hash[hash_key]+",actual:"+ jirarow[key_num]+",id:"+id+",moddate:"+rtcrow[1]
     else
       #puts id+":"+hash[rtcrow[key_num]]+":"+jirarow[key_num]+":"+key_num.to_s
     end
+  end
+  if $debug
+    p id
+    p key_num
+    p hash[hash_key]
+    p jirarow[key_num]
   end
   if msg != nil
     puts msg
@@ -64,7 +70,7 @@ def show_exceptions()
     end
     i += 1
     if $debug
-      exit if i > 10
+      exit if i > 20
     end
   }
 end
@@ -91,46 +97,55 @@ def process_row(rtcrow,jirarow)
     p rtcrow
     p jirarow
   end
-  test_equals(jirarow[idno],cust,rtcrow,jirarow,"Customer")
-  test_dates(jirarow[idno],dued,rtcrow,jirarow,"rtc due date","jira due date")
-  #test_dates(jirarow[idno],resd,rtcrow,jirarow,"rtc resolution date","jira resolved date")
-  # RTC user
-  test_mappings(jirarow[idno],assn,$users,rtcrow,jirarow,"rtc 'assigned to'","jira user")
-  test_mappings(jirarow[idno],crby,$users,rtcrow,jirarow,"rtc 'created by'","jira user")
-  test_mappings(jirarow[idno],qaow,$users,rtcrow,jirarow,"rtc qa owner","jira user")
-
-
   # get RTC resolution
-  test_mappings(jirarow[idno],reso,$resolutions,rtcrow,jirarow,"rtc resolution","jira business impact")
+  #puts "Testing resolution field"
+  if $args =~ /mapping/
+    test_mappings(jirarow[idno],reso,$resolutions,rtcrow,jirarow,"rtc resolution","jira business impact")    
+    # get RTC type
+    test_mappings(jirarow[idno],type,$types,rtcrow,jirarow,"rtc type","jira type")
+      # get RTC priority
+    test_mappings(jirarow[idno],prio,$priorities,rtcrow,jirarow,"rtc priority","jira business impact")
+    # get RTC severity
+    test_mappings(jirarow[idno],seve,$severities,rtcrow,jirarow,"rtc severity","jira priority")
+  end
+    # get RTC component
+  test_equals(jirarow[idno],cust,rtcrow,jirarow,"Customer") if $args =~ /customer/
+  if $args =~ /date/
+    #test_dates(jirarow[idno],dued,rtcrow,jirarow,"rtc due date","jira due date")
+    test_dates(jirarow[idno],resd,rtcrow,jirarow,"rtc resolution date","jira resolved date")
+  end
+
+  # RTC user
+  if $args =~ /user/
+    #puts "user"
+    test_mappings(jirarow[idno],assn,$users,rtcrow,jirarow,"rtc 'assigned to'","jira user")
+    test_mappings(jirarow[idno],crby,$users,rtcrow,jirarow,"rtc 'created by'","jira user")
+    test_mappings(jirarow[idno],qaow,$users,rtcrow,jirarow,"rtc qa owner","jira user")
+  end
+
+
   # get RTC status
-  if $args =~ /component/
+  if $args =~ /status/
     test_mappings(jirarow[idno],stat,$statuses,rtcrow,jirarow,"rtc status","jira status")
   end
-    # get RTC type
-  test_mappings(jirarow[idno],type,$types,rtcrow,jirarow,"rtc type","jira type")
-    # get RTC priority
-  test_mappings(jirarow[idno],prio,$priorities,rtcrow,jirarow,"rtc priority","jira priority")
-  # get RTC severity
-  test_mappings(jirarow[idno],seve,$severities,rtcrow,jirarow,"rtc severity","jira priority")
-    # get RTC component
+
   if $args =~ /component/
     test_mappings(jirarow[idno],comp,$components,rtcrow,jirarow,"rtc component","jira component")
+
   end
       # get RTC version
   if $args =~ /version/
     test_mappings(jirarow[idno],aver,$versions,rtcrow,jirarow,"rtc 'found in'","jira affected version")
     #test_mappings(jirarow[idno],fver,$versions,rtcrow,jirarow,"rtc 'planned for'","jira 'fixed version'")
-    #test_mappings(jirarow[idno],assn,$versions,rtcrow,jirarow,"rtc owned by","jira assignee")
-    #test_mappings(jirarow[idno],crby,$versions,rtcrow,jirarow,"rtc created by","jira reporter")
-    #test_mappings(jirarow[idno],qaow,$versions,rtcrow,jirarow,"rtc QA Owner","jira QA Owner")
     # get RTC browser
-    #test_mappings(jirarow[idno],stat,$browsers,rtcrow,jirarow,"rtc browser","jira browser")
+
   end
+  test_mappings(jirarow[idno],stat,$browsers,rtcrow,jirarow,"rtc browser","jira browser") if $args =~ /browser/
 end
 
 def main
   p $args = ARGV[0]
-  $debug = false
+  $debug = ($args =~ /debug/)
   $rtcrows = read_rtc()
   $jirarows = read_jira()
   puts "rtc:"+$rtcrows.size.to_s
